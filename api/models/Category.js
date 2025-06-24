98581ec8572155638d60f9130421620a84914dde
neeraj1264 <121123848+neeraj1264@users.noreply.github.com>
2025-06-24 11:40:45 +0530

    Update Category.js

--- api/models/Category.js
+++ api/models/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true  },
 });
 
 const Category = mongoose.model('Category', categorySchema);
